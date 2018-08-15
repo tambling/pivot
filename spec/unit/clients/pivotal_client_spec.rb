f8b3c165ee09d470cf49e13effb712b9c99b5d2d
Dean Tambling <dean.tambling@gmail.com>
2018-08-15 12:15:49 -0700

    Add Webmock and test PivotalClient with fixtures

--- spec/unit/clients/pivotal_client_spec.rb
+++ spec/unit/clients/pivotal_client_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 require 'securerandom'
 
+def fixture_file title
+  filename = "#{title}.json"
+  spec_root = '../../..'
+  relative_fixture_path = File.join(spec_root, 'fixtures', 'input', filename)
+  absolute_fixture_path = File.expand_path(relative_fixture_path, __FILE__)
+
+  return File.new(absolute_fixture_path)
+end
+
 RSpec.describe Pivot::PivotalClient do
   it 'can be initialized with a token' do
     token = SecureRandom.hex
@@ -14,58 +23,77 @@ RSpec.describe Pivot::PivotalClient do
     let(:client) { Pivot::PivotalClient.new(token) }
     let(:headers) { { "X-TrackerToken": token } }
 
-    before(:each) do
-      allow(JSON).to receive(:parse)
-    end
-
     describe '#get_projects' do
+      let(:projects_url) { "#{Pivot::PivotalClient::BASE_ROUTE}/projects" }
+
       it "GETs the right URL" do
-        expected_url = "#{Pivot::PivotalClient::BASE_ROUTE}/projects"
-        expect(RestClient).to receive(:get).with(expected_url, anything)
+        expect(RestClient).to receive(:get).with(projects_url, anything).and_return('[]')
 
         client.get_projects
       end
 
       it "has the right headers" do
-        expect(RestClient).to receive(:get).with(anything, headers)
+        expect(RestClient).to receive(:get).with(anything, headers).and_return('[]')
 
         client.get_projects
+      end
+
+      it "returns an array of hashes" do
+        stub_request(:get, projects_url).
+          to_return(body: fixture_file('projects'), status: 200)
+
+        expect(client.get_projects).to all(be_a Hash)
       end
     end
 
     describe '#get_project' do
       let(:project_id) { 1 }
+      let(:project_url) { Addressable::Template.new("#{Pivot::PivotalClient::BASE_ROUTE}/projects/{id}") }
 
       it "GETs the right URL" do
-        expected_url = "#{Pivot::PivotalClient::BASE_ROUTE}/projects/#{project_id}"
-        expect(RestClient).to receive(:get).with(expected_url, anything)
+        expected_url = project_url.expand({id: project_id})
+        expect(RestClient).to receive(:get).with(expected_url, anything).and_return('[]')
 
         client.get_project(project_id)
       end
 
       it "has the right headers" do
-        expect(RestClient).to receive(:get).with(anything, headers)
+        expect(RestClient).to receive(:get).with(anything, headers).and_return('[]')
 
         client.get_project(project_id)
+      end
+
+      it "returns a hash" do
+        stub_request(:get, project_url).
+          to_return(body: fixture_file('project'), status: 200)
+
+        expect(client.get_project(project_id)).to be_a(Hash)
       end
     end
 
     describe '#get_stories' do
       let(:project_id) { 1 }
+      let(:stories_url) { Addressable::Template.new("#{Pivot::PivotalClient::BASE_ROUTE}/projects/{id}/stories") }
 
       it "GETs the right URL" do
-        expected_url = "#{Pivot::PivotalClient::BASE_ROUTE}/projects/#{project_id}/stories"
-        expect(RestClient).to receive(:get).with(expected_url, anything)
+        expected_url = stories_url.expand({id: project_id})
+        expect(RestClient).to receive(:get).with(expected_url, anything).and_return('[]')
 
         client.get_stories(project_id)
       end
 
       it "has the right headers" do
-        expect(RestClient).to receive(:get).with(anything, headers)
+        expect(RestClient).to receive(:get).with(anything, headers).and_return('[]')
 
         client.get_stories(project_id)
       end
 
+      it "returns an array of hashes" do
+        stub_request(:get, stories_url).
+          to_return(body: fixture_file('stories'), status: 200)
+
+        expect(client.get_stories(project_id)).to all(be_a Hash)
+      end
     end
   end
 end
